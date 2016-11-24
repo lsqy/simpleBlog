f100b47ebaa5bc9d6af44ea4288279f25d06a6e2
lsqy <928164042@qq.com>
2016-11-24 23:47:58 +0800

    optimize Bootstrap

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ var Entry = require('./lib/entry');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4009);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('lsqy'));
 app.use(express.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api.auth);
